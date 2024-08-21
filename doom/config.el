86279d8acf663b67f6c96e3320e0cdd6ebe8120f
chigira <98512862+fsetsan@users.noreply.github.com>
2024-08-21 16:19:43 +0900

    Add files via upload

--- doom/config.el
+++ doom/config.el
@@ -32,7 +32,6 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -110,7 +109,7 @@
  "q" (general-key-dispatch 'self-insert-command
        :timeout 0.25
        "q" 'toggle-input-method))
-
+(setq doom-theme 'doom-gruvbox)
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
