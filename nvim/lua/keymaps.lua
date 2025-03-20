d9f28c472c1f6410a4a1ec3f32cb9f4e42358787
fsetsan <stlasasin@gmail.com>
2025-03-20 09:01:13 +0900

    updates

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,166 @@
+vim.g.mapleader = " " -- Leader key set to space
+vim.keymap.set("n", "<leader>n", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>p", ":bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "yy", '"+yy', { noremap = true, silent = true })
+vim.keymap.set("n", "p", '"+p', { noremap = true, silent = true })
+vim.keymap.set("v", "p", '"+p', { noremap = true, silent = true })
+vim.keymap.set("v", "y", '"+y', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<Esc>', '<Cmd>nohlsearch<CR><Esc>', { noremap = true, silent = true })
+-- NvimTree key bindings
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
+vim.keymap.set('n', '<leader>r', ':NvimTreeRefresh<CR>', { desc = 'Refresh NvimTree' })
+vim.keymap.set('n', '<leader>a', ':NvimTreeFindFile<CR>', { desc = 'Find file in NvimTree' })
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Telescope recent files' })
+-- Markdown preview
+vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", { desc = "Toggle Markdown Preview" })
+vim.keymap.set("n", "<leader>an", "<cmd>Anki<CR>", { desc = "Open Anki" })
+-- Spotify control
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>sz",
+  ":!playerctl --player=spotify previous<CR>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>sc",
+  ":!playerctl --player=spotify play-pause<CR>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap("n", "<leader>sx", ":!playerctl --player=spotify next<CR>", { noremap = true, silent = true })
+
+-- Translation key bindings
+vim.api.nvim_set_keymap("n", "<leader>ttj", ":Translate ja<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>ttj", ":Translate ja<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>tte", ":Translate en<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>tte", ":Translate en<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ttf", ":Translate fr<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>ttf", ":Translate fr<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ttk", ":Translate Korean<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>ttk", ":Translate Korean<CR>", { noremap = true, silent = true })
+
+-- ChatGPT and Google Translate shortcuts
+vim.api.nvim_create_user_command("ChatGPT", function()
+  vim.fn.system("xdg-open https://chatgpt.com/")
+end, { desc = "Open ChatGPT in browser" })
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>cg",
+  ":silent !xdg-open https://chatgpt.com/<CR>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>gt",
+  ":silent !xdg-open https://translate.google.com/<CR>",
+  { noremap = true, silent = true }
+)
+
+-- Markdown settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  command = "setlocal nospell",
+})
+
+vim.keymap.set(
+  "n",
+  "<leader>ma",
+  "i[]()<Esc>ha",
+  { noremap = true, silent = true, desc = "Insert []() and move inside" }
+)
+
+-- Run Python script
+vim.api.nvim_set_keymap("n", "<C-n>", ":!python3 %<CR>", { noremap = true, silent = true })
+
+-- Spotify commands
+vim.api.nvim_create_user_command("SpotifyPrev", function()
+  vim.fn.system("playerctl --player=spotify previous")
+end, {})
+
+vim.api.nvim_create_user_command("SpotifyNext", function()
+  vim.fn.system("playerctl --player=spotify next")
+end, {})
+
+vim.api.nvim_create_user_command("SpotifyPlayPause", function()
+  vim.fn.system("playerctl --player=spotify play-pause")
+end, {})
+
+vim.api.nvim_create_user_command("SpotifyStop", function()
+  vim.fn.system("playerctl --player=spotify stop")
+end, {})
+
+vim.api.nvim_create_user_command("SpotifyStatus", function()
+  local status = vim.fn.system("playerctl --player=spotify status")
+  print("Spotify Status: " .. status)
+end, {})
+
+vim.api.nvim_create_user_command("SpotifySong", function()
+  local song = vim.fn.system("playerctl --player=spotify metadata title")
+  print("Now Playing: " .. song)
+end, {})
+
+-- Translate commands
+vim.api.nvim_create_user_command("TranslateJapanese", function(opts)
+  local query = table.concat(opts.fargs, " ")
+  if query == "" then
+    print("Usage: :Translate <text>")
+    return
+  end
+  local output = vim.fn.system("trans :ja " .. vim.fn.shellescape(query))
+  print(output)
+end, { nargs = "+" })
+
+vim.api.nvim_create_user_command("TranslateEnglish", function(opts)
+  local query = table.concat(opts.fargs, " ")
+  if query == "" then
+    print("Usage: :Translate <text>")
+    return
+  end
+  local output = vim.fn.system("trans :en " .. vim.fn.shellescape(query))
+  print(output)
+end, { nargs = "+" })
+
+vim.api.nvim_create_user_command("TranslateKorean", function(opts)
+  local query = table.concat(opts.fargs, " ")
+  if query == "" then
+    print("Usage: :Translate <text>")
+    return
+  end
+  local output = vim.fn.system("trans :ko " .. vim.fn.shellescape(query))
+  print(output)
+end, { nargs = "+" })
+
+vim.api.nvim_create_user_command("TranslateFrench", function(opts)
+  local query = table.concat(opts.fargs, " ")
+  if query == "" then
+    print("Usage: :Translate <text>")
+    return
+  end
+  local output = vim.fn.system("trans :fr " .. vim.fn.shellescape(query))
+  print(output)
+end, { nargs = "+" })
+-- telescope 
+
+local actions = require('telescope.actions')
+local telescope = require('telescope')
+
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = {  -- insertモードでのマッピング
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      },
+      n = {  -- normalモードでも使いたい場合
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      },
+    },
+  },
+})
--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -13,13 +13,252 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Basic options
-vim.g.mapleader = " " -- Leader key set to space
-vim.opt.termguicolors = true
 vim.opt.spell = false
-
--- Plugin specification
+-- Set up plugins
 require("lazy").setup({
-  -- Appearance
+  -- Mason (must come before lspconfig)
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      -- import mason
+      local mason = require("mason")
+      -- enable mason and configure icons
+      mason.setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+
+      -- import mason-lspconfig after mason is set up
+      local mason_lspconfig = require("mason-lspconfig")
+      mason_lspconfig.setup({
+        -- list of servers for mason to install
+        ensure_installed = {
+          "html",
+          "cssls",
+          "tailwindcss",
+          "svelte",
+          "lua_ls",
+          "graphql",
+          "emmet_ls",
+          "prismals",
+          "pyright",
+        },
+      })
+
+      -- Set up mason-tool-installer
+      local mason_tool_installer = require("mason-tool-installer")
+      mason_tool_installer.setup({
+        ensure_installed = {
+          "prettier", -- prettier formatter
+          "stylua", -- lua formatter
+          "isort", -- python formatter
+          "black", -- python formatter
+          "pylint",
+          "eslint_d",
+        },
+      })
+    end,
+    priority = 1000, -- Make sure mason loads before lspconfig
+  },
+
+  -- LSP config (must come after mason)
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      { "antosha417/nvim-lsp-file-operations", config = true },
+      { "folke/neodev.nvim", opts = {} },
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      -- import lspconfig plugin
+      local lspconfig = require("lspconfig")
+      -- import mason_lspconfig plugin
+      local mason_lspconfig = require("mason-lspconfig")
+      -- import cmp-nvim-lsp plugin
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+      local keymap = vim.keymap -- for conciseness
+      
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          local opts = { buffer = ev.buf, silent = true }
+          -- set keybinds
+          opts.desc = "Show LSP references"
+          keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+          opts.desc = "Go to declaration"
+          keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+          opts.desc = "Show LSP definitions"
+          keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+          opts.desc = "Show LSP implementations"
+          keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+          opts.desc = "Show LSP type definitions"
+          keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+          opts.desc = "See available code actions"
+          keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+          opts.desc = "Smart rename"
+          keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+          opts.desc = "Show buffer diagnostics"
+          keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+          opts.desc = "Show line diagnostics"
+          keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+          opts.desc = "Go to previous diagnostic"
+          keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+          opts.desc = "Go to next diagnostic"
+          keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+          opts.desc = "Show documentation for what is under cursor"
+          keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+          opts.desc = "Restart LSP"
+          keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+        end,
+      })
+      
+      -- used to enable autocompletion (assign to every lsp server config)
+      local capabilities = cmp_nvim_lsp.default_capabilities()
+      
+      -- Change the Diagnostic symbols in the sign column (gutter)
+      local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      end
+      
+      mason_lspconfig.setup_handlers({
+        -- default handler for installed servers
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+        ["svelte"] = function()
+          -- configure svelte server
+          lspconfig["svelte"].setup({
+            capabilities = capabilities,
+            on_attach = function(client, bufnr)
+              vim.api.nvim_create_autocmd("BufWritePost", {
+                pattern = { "*.js", "*.ts" },
+                callback = function(ctx)
+                  -- Here use ctx.match instead of ctx.file
+                  client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+                end,
+              })
+            end,
+          })
+        end,
+        ["graphql"] = function()
+          -- configure graphql language server
+          lspconfig["graphql"].setup({
+            capabilities = capabilities,
+            filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+          })
+        end,
+        ["emmet_ls"] = function()
+          -- configure emmet language server
+          lspconfig["emmet_ls"].setup({
+            capabilities = capabilities,
+            filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+          })
+        end,
+        ["lua_ls"] = function()
+          -- configure lua server (with special settings)
+          lspconfig["lua_ls"].setup({
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                -- make the language server recognize "vim" global
+                diagnostics = {
+                  globals = { "vim" },
+                },
+                completion = {
+                  callSnippet = "Replace",
+                },
+              },
+            },
+          })
+        end,
+      })
+    end,
+  },
+
+  -- Completion
+ {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+    -- ADD THIS:
+    "uga-rosa/cmp-dictionary",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    -- Dictionary setup
+    require("cmp_dictionary").setup({
+      paths = { "/usr/share/dict/french"},
+      exact_length = 2,
+    })
+
+    cmp.setup({
+      completion = {
+        completeopt = "menu,menuone,preview,noselect",
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-a>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = {
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'path' },
+        -- ADD THIS:
+        { name = "dictionary", keyword_length = 2 },
+      },
+    })
+  end,
+},
+  -- Appearance plugins
   {
     "ellisonleao/gruvbox.nvim",
     priority = 1000, -- Make sure colorscheme loads first
@@ -27,10 +266,6 @@ require("lazy").setup({
       vim.cmd.colorscheme "gruvbox"
     end,
   },
-{
-  "github/copilot.vim",
-  lazy=false,
-},
   {
     'goolord/alpha-nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -54,71 +289,6 @@ require("lazy").setup({
     end
   },
   
-  -- LSP and completion
-  {
-    "williamboman/mason.nvim",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "clangd" },
-        automatic_installation = true,
-      })
-      
-      -- Set up LSP handlers
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
-          })
-        end,
-      })
-      
-      -- Specific LSP configurations
-      require('lspconfig').pyright.setup{}
-    end
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-vsnip",
-      "hrsh7th/vim-vsnip",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping.select_next_item(),
-          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'buffer' },
-          { name = 'path' },
-        })
-      })
-    end
-  },
-  
   -- File explorer
   {
     'nvim-tree/nvim-tree.lua',
@@ -133,14 +303,6 @@ require("lazy").setup({
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Telescope old files' })
-    end
   },
   
   -- Which-key (key binding helper)
@@ -189,158 +351,14 @@ require("lazy").setup({
   },
   {
     'iamcco/markdown-preview.nvim',
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
+build = "cd app && npm install",
+lazy = false,
+  config = function()
+    vim.g.mkdp_auto_start = 1
+    end
   },
-  
   -- Anki integration
   {
-    "rareitems/anki.nvim",
-    config = function()
-      require("anki").setup({
-        tex_support = false,
-        models = {
-          ["Basic"] = "Deck",
-          ["Super Basic"] = "Deck::ChildDeck",
-        },
-      })
-    end,
-  },
+'rolf-stargate/ankifly.nvim'
+  }
 })
-
--- Clipboard integration
-vim.keymap.set("n", "yy", '"+yy', { noremap = true, silent = true })
-vim.keymap.set("v", "y", '"+y', { noremap = true, silent = true })
-
--- NvimTree key bindings
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
-vim.keymap.set('n', '<leader>r', ':NvimTreeRefresh<CR>', { desc = 'Refresh NvimTree' })
-vim.keymap.set('n', '<leader>n', ':NvimTreeFindFile<CR>', { desc = 'Find file in NvimTree' })
-vim.keymap.set('n', '<leader>a', ':NvimTreeOldFile<CR>', { desc = 'Open Old file in NvimTree' })
-
--- Spotify control
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>sz",
-  ":!playerctl --player=spotify previous<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>sc",
-  ":!playerctl --player=spotify play-pause<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap("n", "<leader>sx", ":!playerctl --player=spotify next<CR>", { noremap = true, silent = true })
-
--- Translation key bindings
-vim.api.nvim_set_keymap("n", "<leader>ttj", ":Translate ja<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>ttj", ":Translate ja<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>tte", ":Translate en<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>tte", ":Translate en<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>ttf", ":Translate fr<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>ttf", ":Translate fr<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>ttk", ":Translate Korean<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>ttk", ":Translate Korean<CR>", { noremap = true, silent = true })
-
--- ChatGPT and Google Translate shortcuts
-vim.api.nvim_create_user_command("ChatGPT", function()
-  vim.fn.system("xdg-open https://chatgpt.com/")
-end, { desc = "Open ChatGPT in browser" })
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>cg",
-  ":silent !xdg-open https://chatgpt.com/<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>gt",
-  ":silent !xdg-open https://translate.google.com/<CR>",
-  { noremap = true, silent = true }
-)
-
--- Markdown settings
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  command = "setlocal nospell",
-})
-
-vim.keymap.set(
-  "n",
-  "<leader>ma",
-  "i[]()<Esc>ha",
-  { noremap = true, silent = true, desc = "Insert []() and move inside" }
-)
-
--- Run Python script
-vim.api.nvim_set_keymap("n", "<C-n>", ":!python3 %<CR>", { noremap = true, silent = true })
-
--- Spotify commands
-vim.api.nvim_create_user_command("SpotifyPrev", function()
-  vim.fn.system("playerctl --player=spotify previous")
-end, {})
-
-vim.api.nvim_create_user_command("SpotifyNext", function()
-  vim.fn.system("playerctl --player=spotify next")
-end, {})
-
-vim.api.nvim_create_user_command("SpotifyPlayPause", function()
-  vim.fn.system("playerctl --player=spotify play-pause")
-end, {})
-
-vim.api.nvim_create_user_command("SpotifyStop", function()
-  vim.fn.system("playerctl --player=spotify stop")
-end, {})
-
-vim.api.nvim_create_user_command("SpotifyStatus", function()
-  local status = vim.fn.system("playerctl --player=spotify status")
-  print("Spotify Status: " .. status)
-end, {})
-
-vim.api.nvim_create_user_command("SpotifySong", function()
-  local song = vim.fn.system("playerctl --player=spotify metadata title")
-  print("Now Playing: " .. song)
-end, {})
-
--- Translate commands
-vim.api.nvim_create_user_command("TranslateJapanese", function(opts)
-  local query = table.concat(opts.fargs, " ")
-  if query == "" then
-    print("Usage: :Translate <text>")
-    return
-  end
-  local output = vim.fn.system("trans :ja " .. vim.fn.shellescape(query))
-  print(output)
-end, { nargs = "+" })
-
-vim.api.nvim_create_user_command("TranslateEnglish", function(opts)
-  local query = table.concat(opts.fargs, " ")
-  if query == "" then
-    print("Usage: :Translate <text>")
-    return
-  end
-  local output = vim.fn.system("trans :en " .. vim.fn.shellescape(query))
-  print(output)
-end, { nargs = "+" })
-
-vim.api.nvim_create_user_command("TranslateKorean", function(opts)
-  local query = table.concat(opts.fargs, " ")
-  if query == "" then
-    print("Usage: :Translate <text>")
-    return
-  end
-  local output = vim.fn.system("trans :ko " .. vim.fn.shellescape(query))
-  print(output)
-end, { nargs = "+" })
-
-vim.api.nvim_create_user_command("TranslateFrench", function(opts)
-  local query = table.concat(opts.fargs, " ")
-  if query == "" then
-    print("Usage: :Translate <text>")
-    return
-  end
-  local output = vim.fn.system("trans :fr " .. vim.fn.shellescape(query))
-  print(output)
-end, { nargs = "+" })
