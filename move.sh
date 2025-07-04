760126c3ea808bef4c5aff6e161e726a5f17e18c
q <errrsss@qs-MacBook-Air.local>
2025-07-04 18:58:35 +0900

    updates

--- move.sh
+++ move.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd ~/.config && cp -r nvim ghostty aerospace karabiner ~/dotfiles
+cd ~/.config && cp -r nvim ghostty aerospace karabiner fish ~/dotfiles && cd ~/dotfiles/
 # For archlinux
 #cd ~/.config && cp -r nvim ghostty i3wm ~/dotfiles
